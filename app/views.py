093e400f0f14ed9a4335c7c5d6f28d4932e1f775
vishal <narwadevishal147@gmail.com>
2025-03-16 09:46:48 +0530

    new commit

--- app/views.py
+++ app/views.py
@@ -5,16 +5,19 @@
 
 def add_numbers(a,b):
     res = int(a)+int(b)
+    print(f"{a}+{b}= ", res)
     return res
 
 
 def subtract_numbers(a,b):
     res = a-b
+    print(f"{a}-{b}= ", res)
     return res
 
 
 def multiply_numbers(a,b):
     res = a*b
+    print(f"{a}*{b}= ", res)
     return res
 
 
@@ -23,6 +26,7 @@ def division_numbers(a,b):
         print("Can't divide by Zero, provide valid number")
     else:
         res = a/b
+        print(f"{a}/{b}= ", res)
         return res
 
 
@@ -30,3 +34,4 @@ if __name__ == "__main__":
     add_numbers(2,3)
     subtract_numbers(40, 20)
     multiply_numbers(12, 10)
+    division_numbers(10,5)
